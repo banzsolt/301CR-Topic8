693f6e8d2304c45fc261bc034a86f168aaead1ff
Zsolt Ban <banzsolt94@gmail.com>
2016-12-12 12:13:49 +0000

    Implemented boost JSON for read and write

--- 301CR-Topic8/jsonParser.cpp
+++ 301CR-Topic8/jsonParser.cpp
@@ -5,6 +5,9 @@
 #include <boost/property_tree/json_parser.hpp>
 
 using namespace std;
+// Short alias for this namespace
+//Refference: http://zenol.fr/blog/boost-property-tree/en.html
+namespace pt = boost::property_tree;
 
 JsonParser::JsonParser()
 {
@@ -12,53 +15,53 @@ JsonParser::JsonParser()
 
 void JsonParser::WriteToFile(string filename, Player player)
 {
-	ofstream myfile;
-	myfile.open(filename);
+	//ofstream myfile;
+	//myfile.open(filename);
 
-	myfile << "{\n";
-	myfile << "\"id\":" << player.id << ",\n";
-	myfile << "\"name\":\"" << player.name << "\",\n";
-	myfile << "\"dateOfBirth\":" << player.dateOfBirth << ",\n";
-	myfile << "\"country\":\"" << player.country << "\",\n";
-	myfile << "\"email\":\"" << player.email << "\",\n";
-	myfile << "\"gamedata\": { \n";
-		myfile << "\"scoreInLevels\": [ \n";
-			int iterator = 0;
-			for each (int score in player.playerGameData.scoreInLevels)
-			{			
-				iterator++;
-				if (iterator != player.playerGameData.scoreInLevels.size())
-				{
-					myfile << score << ",\n";
-				}
-				else
-				{
-					myfile << score << "\n";
-				}
-			}
-		myfile << "],\n";
-		myfile << "\"currentLevel\": " << player.playerGameData.currentLevel << ",\n";
-		myfile << "\"remainingLives\": " << player.playerGameData.remainingLives << ",\n";
-		myfile << "\"bulletsCount\": " << player.playerGameData.bulletsCount << ",\n";
-		myfile << "\"maxLevelReached\": " << player.playerGameData.maxLevelReached << " \n";
-		myfile << "},\n";
-	myfile << "\"preference\": { \n";
-		myfile << "\"language\":\"" << player.playerPreference.language << "\",\n";
-		myfile << "\"volumeLevel\":" << player.playerPreference.volumeLevel << ",\n";
-		myfile << "\"graphicQuality\":\"" << player.playerPreference.graphicQuality << "\",\n";
-		myfile << "\"difficultyLevel\":" << player.playerPreference.difficultyLevel << "\n";
-		myfile << "}\n";
-	myfile << "}\n";
-	
-	myfile.close();
+	pt::ptree root;
+
+	root.put("id", player.id);
+	root.put("name", player.name);
+	root.put("dateOfBirth", player.dateOfBirth);
+	root.put("country", player.country);
+	root.put("email", player.email);
+
+	pt::ptree playerGameData;
+	// Add a levelScores
+	pt::ptree levelScores;
+	for (auto &score : player.playerGameData.scoreInLevels)
+	{
+		// Create an unnamed node containing the value
+		pt::ptree fruit_node;
+		fruit_node.put("", score);
+
+		// Add this node to the list.
+		levelScores.push_back(std::make_pair("", fruit_node));
+	}
+	playerGameData.add_child("scoreInLevels", levelScores);
+	playerGameData.put("currentLevel", player.playerGameData.currentLevel);
+	playerGameData.put("remainingLives", player.playerGameData.remainingLives);
+	playerGameData.put("bulletsCount", player.playerGameData.bulletsCount);
+	playerGameData.put("maxLevelReached", player.playerGameData.maxLevelReached);
+
+	root.add_child("gamedata", playerGameData);
+
+	pt::ptree playerPreference;
+	playerPreference.put("language", player.playerPreference.language);
+	playerPreference.put("volumeLevel", player.playerPreference.volumeLevel);
+	playerPreference.put("graphicQuality", player.playerPreference.graphicQuality);
+	playerPreference.put("difficultyLevel", player.playerPreference.difficultyLevel);
+
+	root.add_child("preference", playerPreference);
+
+
+	pt::write_json(std::cout, root);
+	pt::write_json(filename, root);
 
 }
 
 Player JsonParser::ReadFromFile(string filename)
 {
-	// Short alias for this namespace
-	namespace pt = boost::property_tree;
-
 	// Create a root
 	pt::ptree root;
 	pt::read_json(filename, root);
@@ -74,18 +77,24 @@ Player JsonParser::ReadFromFile(string filename)
 	result.email = root.get<string>("email", "");
 
 	pt::ptree gameData;
-	pt::read_json(filename, gameData);	
-	result.playerGameData.currentLevel = gameData.get<int>("currentLevel", 0);
-	result.playerGameData.remainingLives = gameData.get<int>("remainingLives", 0);
-	result.playerGameData.bulletsCount = gameData.get<int>("bulletsCount", 0);
-	result.playerGameData.maxLevelReached = gameData.get<int>("maxLevelReached", 0);
+	//pt::read_json(filename, gameData);	
+	result.playerGameData.currentLevel = root.get<int>("gamedata.currentLevel", 0);
+	result.playerGameData.remainingLives = root.get<int>("gamedata.remainingLives", 0);
+	result.playerGameData.bulletsCount = root.get<int>("gamedata.bulletsCount", 0);
+	result.playerGameData.maxLevelReached = root.get<int>("gamedata.maxLevelReached", 0);
+
+	for (pt::ptree::value_type &score : root.get_child("gamedata.scoreInLevels"))
+	{
+		result.playerGameData.scoreInLevels.push_back(stoi(score.second.data()));
+	}
+
 
 	pt::ptree playerPreference;
 	pt::read_json(filename, playerPreference);
-	result.playerPreference.language = playerPreference.get<string>("language", "");
-	result.playerPreference.volumeLevel = playerPreference.get<int>("volumeLevel", 0);
-	result.playerPreference.graphicQuality = playerPreference.get<string>("graphicQuality", "");
-	result.playerPreference.difficultyLevel = playerPreference.get<int>("difficultyLevel", 0);	
+	result.playerPreference.language = root.get<string>("preference.language", "");
+	result.playerPreference.volumeLevel = root.get<int>("preference.volumeLevel", 0);
+	result.playerPreference.graphicQuality = root.get<string>("preference.graphicQuality", "");
+	result.playerPreference.difficultyLevel = root.get<int>("preference.difficultyLevel", 0);
 
 	return result;
 }
